c829b615ea30029aee24c7e57a03f0fda0fc1bd0
Ma][ <testmail@ya.ru>
2022-11-10 21:37:41 +0500

    add Evaluator.java

---
+++ src/Evaluator.java
@@ -0,0 +1,23 @@
+public class Evaluator {
+    private String expression;               //incoming expression
+    private String RPN;                     // expression converted to Rear polish notation.
+    private Double answer;                  // calculated answer from RPN.
+
+   public double evaluate(String expression){
+       this.expression = expression;
+       return 0;
+   }
+
+    private String getExpressionToRPN(String expression){
+
+        return null;
+    }
+
+    private double rpnToAnswer(String rpn){
+        return 0;
+    }
+
+    private int getPriority(char symbol){
+        return 0;
+    }
+}
